b224995022c444ae044b3eff11d2b68b66251319
elbrahian <brahianrueda123@gmial.com>
2024-05-23 11:41:53 -0500

    clase 22 mayo 2.0 - reponse controller, paisresponse, ConsultarPaisesFachadaImpl, ConsultarPaisesFachada.java, ConsultarPaisesImpl

--- src/main/java/co/edu/uco/tiendachepito/api/respose/pais/PaisResponse.java
+++ src/main/java/co/edu/uco/tiendachepito/api/respose/pais/PaisResponse.java
@@ -9,17 +9,25 @@ import java.util.List;
 
 public class PaisResponse extends Response<PaisDTO> {
 
-    public static final Response<PaisDTO> build(List<String> mensajes, List<PaisDTO> datos){
-        Response<PaisDTO> instance = new PaisResponse();
+    public static final PaisResponse build(List<String> mensajes, List<PaisDTO> datos){
+        PaisResponse instance = new PaisResponse();
         instance.setMensajes(mensajes);
         instance.setDatos(datos);
 
         return instance;
     }
-    public static final Response<PaisDTO> build(List<PaisDTO> datos){
-        Response<PaisDTO> instance = new PaisResponse();
+    public static final PaisResponse build(List<PaisDTO> datos){
+        PaisResponse instance = new PaisResponse();
         instance.setMensajes(new ArrayList<>());
         instance.setDatos(datos);
+
+        return instance;
+    }
+
+    public static final PaisResponse build() {
+        PaisResponse instance = new PaisResponse();
+        instance.setMensajes(new ArrayList<>());
+        instance.setDatos(new ArrayList<>());
 
         return instance;
     }
